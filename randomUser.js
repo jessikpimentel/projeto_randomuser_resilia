54c0925de1d9a96c8266c56ebb5b175472a2b952
jessikpimentel <jessikpimentel@hotmail.com>
2020-07-06 11:02:22 -0300

    ätualização da implementação

--- randomUser.js
+++ randomUser.js
@@ -1,11 +1,15 @@
 let request = new XMLHttpRequest();
 
+let field = document.querySelector("#field");
+let button = document.querySelector("#button");
+
 request.open("GET", "https://randomuser.me/api/");
 
 request.addEventListener("load",function(){
+    let objetoApi = JSON.parse(request.responseText);
+
     let nome = document.createElement("p");
     document.body.appendChild(nome);
-    let objetoApi = JSON.parse(request.responseText);
 
     nome.textContent= objetoApi.results[0].name.first+ " " +objetoApi.results[0].name.last;
 
@@ -27,4 +31,47 @@ request.addEventListener("load",function(){
 
 
 });
+
+button.addEventListener("click",function(){
+    let newRequest = new XMLHttpRequest();
+    let nameUrl = "https://randomuser.me/api?results=" + field.value;
+
+    newRequest.open("GET", nameUrl);
+    newRequest.addEventListener("load",function(event){
+        let newObjectApi = JSON.parse(newRequest.responseText);
+
+        event.preventDefault();
+
+        for(let i=0; i<newObjectApi.results.length; i++){
+
+            let blankLine = document.createElement("br");
+            document.body.appendChild(blankLine);
+            let nome = document.createElement("p");
+            nome.classList.add("distance");
+            document.body.appendChild(nome);
+        
+            nome.textContent= newObjectApi.results[i].name.first+ " " +newObjectApi.results[i].name.last;
+        
+            let gender = document.createElement("p");
+            document.body.appendChild(gender);
+        
+            gender.textContent= newObjectApi.results[i].gender;
+        
+            let picture = document.createElement("img");
+            document.body.appendChild(picture);
+            picture.src= newObjectApi.results[i].picture.medium;
+            picture.classList.add("picture");
+        
+        
+            let country = document.createElement("p");
+            document.body.appendChild(country);
+        
+            country.textContent= newObjectApi.results[i].location.country; 
+        }
+
+    });
+
+    newRequest.send();
+});
+
 request.send();
