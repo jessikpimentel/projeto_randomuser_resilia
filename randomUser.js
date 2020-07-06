dc82055e387afefeb2ebeb4b3affb776fd1ed4a9
jessikpimentel <jessikpimentel@hotmail.com>
2020-07-06 11:22:53 -0300

    alteração

--- randomUser.js
+++ randomUser.js
@@ -1,36 +1,8 @@
-let request = new XMLHttpRequest();
+
 
 let field = document.querySelector("#field");
 let button = document.querySelector("#button");
 
-request.open("GET", "https://randomuser.me/api/");
-
-request.addEventListener("load",function(){
-    let objetoApi = JSON.parse(request.responseText);
-
-    let nome = document.createElement("p");
-    document.body.appendChild(nome);
-
-    nome.textContent= objetoApi.results[0].name.first+ " " +objetoApi.results[0].name.last;
-
-    let gender = document.createElement("p");
-    document.body.appendChild(gender);
-
-    gender.textContent= objetoApi.results[0].gender;
-
-    let picture = document.createElement("img");
-    document.body.appendChild(picture);
-    picture.src= objetoApi.results[0].picture.medium;
-    picture.classList.add("picture");
-
-
-    let country = document.createElement("p");
-    document.body.appendChild(country);
-
-    country.textContent= objetoApi.results[0].location.country;
-
-
-});
 
 button.addEventListener("click",function(){
     let newRequest = new XMLHttpRequest();
@@ -73,5 +45,3 @@ button.addEventListener("click",function(){
 
     newRequest.send();
 });
-
-request.send();
